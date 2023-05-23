e9a2c41ad7ce358f602f2c491a70c9f8b0777c8f
dheeraj <dheerajck18@gmail.com>
2023-05-24 00:07:08 +0530

    Refactored main.py

--- main.py
+++ main.py
@@ -12,38 +12,37 @@ available_browser_functions = {
 }
 
 
-def get_session_from_browser(SELECTED_BROWSER):
-    BARD_SESSION = ""
+def get_session_from_browser():
+    print("BARD_SESSION not found on env file\n")
+    SELECTED_BROWSER = config('SELECTED_BROWSER', default="").strip()
     if SELECTED_BROWSER == "":
-        raise ValueError("SELECTED_BROWSER not found on env file")
+        raise ValueError("You need to specify either BARD_SESSION or SELECTED_BROWSER in the env file")
+
+    BARD_SESSION = ""
 
     browser_function = available_browser_functions.get(SELECTED_BROWSER)
     if browser_function is None:
         available_chocies = ', '.join(available_browser_functions.keys())
         raise ValueError(f"Invalid browser selected, available choices are {available_chocies}")
 
+    print(
+        f"Trying to retrieve bard session from {SELECTED_BROWSER}, this only works if you have logged into https://bard.google.com/ from {SELECTED_BROWSER} in non-incognito mode\n"
+    )
+
     cookies = browser_function(domain_name="google.com")
 
     for cookie in cookies:
         if cookie.name == '__Secure-1PSID':
-            BARD_SESSION = cookie.value
-            print("Retrieved session file\n")
-            break
+            if cookie.value != "":
+                BARD_SESSION = cookie.value
+                print("Retrieved session file\n")
+                break
 
     return BARD_SESSION
 
 
 def get_chatbot():
-    BARD_SESSION = config('BARD_SESSION', default="")
-    if BARD_SESSION == "":
-        print("BARD_SESSION not found on env file\n")
-
-        SELECTED_BROWSER = config('SELECTED_BROWSER', default="").strip()
-        print(
-            f"Trying to retrieve bard session from {SELECTED_BROWSER}, this only works if you have logged into https://bard.google.com/ from {SELECTED_BROWSER}(not in incognito mode)\n"
-        )
-
-        BARD_SESSION = get_session_from_browser(SELECTED_BROWSER)
+    BARD_SESSION = config('BARD_SESSION', default="") or get_session_from_browser()
 
     if BARD_SESSION:
         chatbot = Chatbot(BARD_SESSION)
